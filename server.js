dc913d814d34b4106518208225617f5021d1bbc9
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2020-12-31 15:05:01 -0500

    debugging

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3002;
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+mongoose.connect(MONGODB_URI ,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false }
 );
 
 
